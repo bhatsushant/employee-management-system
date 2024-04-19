e2eff492518184742b24b2ab6b2a3dd6c8a5ee08
Sushant Bhat <sush1393@gmail.com>
2024-04-19 17:31:57 -0400

    add google font to the project

--- client/src/pages/EmployeeDetails.tsx
+++ client/src/pages/EmployeeDetails.tsx
@@ -1,12 +1,23 @@
+import * as React from "react";
 import { useLocation } from "react-router-dom";
-import { useEmployeeContext } from "@/contexts/EmployeeContext";
 import { useEffect, useState } from "react";
 import { Employee } from "@/models/employee";
+import { Button } from "@/components/ui/button";
+import ConfirmationModal from "@/components/ConfirmationModal";
+import { useNavigate } from "react-router-dom";
+import { useEmployeeContext } from "@/contexts/EmployeeContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function EmployeeDetails() {
-  const { getEmployee } = useEmployeeContext();
+  const { getEmployee, deleteEmployee } = useEmployeeContext();
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [employee, setEmployee] = useState<Employee | undefined>(undefined);
+  const [showDeleteModal, setShowDeleteModal] = React.useState<boolean>(false);
+  const [employeeToDelete, setEmployeeToDelete] =
+    React.useState<Employee | null>(null);
 
   useEffect(() => {
     const fetchEmployeeData = async () => {
@@ -30,12 +41,57 @@ export function EmployeeDetails() {
     fetchEmployeeData();
   }, [location.state, getEmployee]);
 
+  const handleEditEmployee = (employee: Employee) => {
+    navigate("/employee_form", { state: { employee, isEdit: true } });
+  };
+
+  const handleDeleteEmployee = (employee: Employee) => {
+    setEmployeeToDelete(employee);
+    setShowDeleteModal(true);
+  };
+
+  const confirmDelete = async () => {
+    if (employeeToDelete) {
+      const deleted = await deleteEmployee(employeeToDelete.employeeId);
+      if (!deleted.success) {
+        toast.error(deleted.message, {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          progress: undefined
+        });
+      } else {
+        toast.success(deleted.message, {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          progress: undefined
+        });
+        setEmployee(undefined);
+        navigate("/dashboard");
+      }
+
+      setShowDeleteModal(false);
+      setEmployeeToDelete(null);
+    }
+  };
+
+  const closeModal = () => {
+    setShowDeleteModal(false);
+    setEmployeeToDelete(null);
+  };
+
   if (!employee) {
     return <div className="text-red-600">Employee not found!</div>;
   }
 
   return (
-    <div className="max-w-lg mx-auto shadow-md rounded px-8 pt-6">
+    <div className="max-w-lg mx-auto shadow-md rounded px-8 pt-6 pb-8">
+      <ToastContainer />
       <div className="mb-4">
         <img
           src={employee.image}
@@ -87,6 +143,17 @@ export function EmployeeDetails() {
       <div className="mb-4">
         <strong className="mr-2">Is Employed:</strong>{" "}
         {employee.isEmployed ? "Yes" : "No"}
+      </div>
+
+      <div className="w-full flex gap-x-7">
+        <Button onClick={() => handleEditEmployee(employee)}>Edit</Button>
+        <Button onClick={() => handleDeleteEmployee(employee)}>Delete</Button>
+        <ConfirmationModal
+          isOpen={showDeleteModal}
+          onClose={closeModal}
+          onConfirm={confirmDelete}
+          message="Are you sure you want to delete this employee?"
+        />
       </div>
     </div>
   );
